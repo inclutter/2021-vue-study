ee9e9a2633ec3f0a97c7610fd636a906fd1d0b85
강민우 <augustman@knou.ac.kr>
2021-10-31 08:01:15 +0900

    추가

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
+const fs = require('fs')
+const path = require("path");
 module.exports = {
     // options...
+
     devServer: {
         disableHostCheck: true,
+        https: {
+            key: fs.readFileSync(path.join(__dirname, 'data/cert/prv_www.inclutter.com.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'data/cert/cert_www.inclutter.com.crt')),
+            ca: fs.readFileSync(path.join(__dirname, 'data/cert/rootca_www.inclutter.com.crt')),
+        },
     }
 };
