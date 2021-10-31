3382ad960005d9bb27e6847d8e462a4b22e5b939
강민우 <augustman@knou.ac.kr>
2021-10-31 11:32:50 +0900

    ㅅㅈ

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,10 @@ module.exports = {
 
     devServer: {
         disableHostCheck: true,
-        https: false
+        https: {
+            key: fs.readFileSync(path.join(__dirname, 'data/cert/prv_www.inclutter.com.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'data/cert/cert_www.inclutter.com.crt')),
+            ca: fs.readFileSync(path.join(__dirname, 'data/cert/rootca_www.inclutter.com.crt')),
+        },
     }
 };
