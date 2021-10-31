844dc351652d7210d9d35a39013f999f5863e324
강민우 <augustman@knou.ac.kr>
2021-10-31 11:35:31 +0900

    ㅅㅈ

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,7 @@ module.exports = {
             cert: fs.readFileSync(path.join(__dirname, 'data/cert/cert_www.inclutter.com.crt')),
             ca: fs.readFileSync(path.join(__dirname, 'data/cert/rootca_www.inclutter.com.crt')),
         },
+        host: 'inclutter.com',
+        port: 8082
     }
 };
