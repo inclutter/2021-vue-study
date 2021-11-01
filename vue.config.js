da04909ca80a4b6f26755a6e313e62a851f9fbea
mwkang <augustman@knou.ac.kr>
2021-11-01 12:10:55 +0900

    no message

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,10 @@ module.exports = {
 
     devServer: {
         disableHostCheck: true,
-        // https: {
-        //     key: fs.readFileSync(path.join(__dirname, 'data/cert/prv_www.inclutter.com.key')),
-        //     cert: fs.readFileSync(path.join(__dirname, 'data/cert/cert_www.inclutter.com.crt')),
-        //     ca: fs.readFileSync(path.join(__dirname, 'data/cert/rootca_www.inclutter.com.crt')),
-        //     csr: fs.readFileSync(path.join(__dirname, 'data/cert/csr_www.inclutter.com.txt')),
-        //     subcal: fs.readFileSync(path.join(__dirname, 'data/cert/subca1_www.inclutter.com.crt')),
-        // },
-        https: true,
-        host: 'inclutter.com',
+        https: {
+            key: fs.readFileSync(path.join(__dirname, 'data/cert/pub_www.inclutter.com.key')),
+            cert: fs.readFileSync(path.join(__dirname, 'data/cert/cert_www.inclutter.com.crt')),
+            ca: fs.readFileSync(path.join(__dirname, 'data/cert/rootca_www.inclutter.com.crt')),
+        },
     }
 };
