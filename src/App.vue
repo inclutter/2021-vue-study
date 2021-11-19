d260de64797edb07a6c8309ee42a17596341d06a
강민우 <augustman@knou.ac.kr>
2021-11-19 19:31:15 +0900

    vuetify side bar icon 처리 및 수정

--- src/App.vue
+++ src/App.vue
@@ -5,112 +5,85 @@
         color="primary"
         dark
     >
-      <div class="d-flex align-center">
-        <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-        />
 
-        <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-            width="100"
-        />
-      </div>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer>
 
-      <v-spacer></v-spacer>
+      </v-spacer>
 
-      <v-btn
-          href="https://github.com/vuetifyjs/vuetify/releases/latest"
-          target="_blank"
-          text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
-    <div id="app">
-      <v-navigation-drawer
-          absolute
-          dark
-          src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
-          width="10%"
-          permanent
-      >
-        <v-list>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
+    <v-navigation-drawer
+        dark
+        src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        v-model="drawer"
+        app
+    >
+      <v-list>
+        <v-list-item>
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
 
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-group
+            :value="true"
+            prepend-icon="mdi-account-circle"
+        >
+          <template v-slot:activator>
+            <v-list-item-title>Users</v-list-item-title>
+          </template>
 
           <v-list-group
               :value="true"
-              prepend-icon="mdi-account-circle"
+              no-action
+              sub-group
           >
             <template v-slot:activator>
-              <v-list-item-title>Users</v-list-item-title>
+              <v-list-item-content>
+                <v-list-item-title>Admin</v-list-item-title>
+              </v-list-item-content>
             </template>
 
-            <v-list-group
-                :value="true"
-                no-action
-                sub-group
+            <v-list-item
+                v-for="([title, icon], i) in admins"
+                :key="i"
+                link
             >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title>Admin</v-list-item-title>
-                </v-list-item-content>
-              </template>
+              <v-list-item-title v-text="title"></v-list-item-title>
 
-              <v-list-item
-                  v-for="([title, icon], i) in admins"
-                  :key="i"
-                  link
-              >
-                <v-list-item-title v-text="title"></v-list-item-title>
-
-                <v-list-item-icon>
-                  <v-icon v-text="icon"></v-icon>
-                </v-list-item-icon>
-              </v-list-item>
-            </v-list-group>
-
-            <v-list-group
-                no-action
-                sub-group
-            >
-              <template v-slot:activator>
-                <v-list-item-content>
-                  <v-list-item-title>Actions</v-list-item-title>
-                </v-list-item-content>
-              </template>
-
-              <v-list-item
-                  v-for="([title, icon], i) in cruds"
-                  :key="i"
-                  link
-              >
-                <v-list-item-title v-text="title"></v-list-item-title>
-
-                <v-list-item-icon>
-                  <v-icon v-text="icon"></v-icon>
-                </v-list-item-icon>
-              </v-list-item>
-            </v-list-group>
+              <v-list-item-icon>
+                <v-icon v-text="icon"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
           </v-list-group>
-        </v-list>
-      </v-navigation-drawer>
-    </div>
+
+          <v-list-group
+              no-action
+              sub-group
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>Actions</v-list-item-title>
+              </v-list-item-content>
+            </template>
+
+            <v-list-item
+                v-for="([title, icon], i) in cruds"
+                :key="i"
+                link
+            >
+              <v-list-item-title v-text="title"></v-list-item-title>
+
+              <v-list-item-icon>
+                <v-icon v-text="icon"></v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </v-list-group>
+        </v-list-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
       <HelloWorld/>
     </v-main>
@@ -128,6 +101,7 @@ export default {
   },
 
   data: () => ({
+    drawer: false,
     admins: [
       ['Management', 'mdi-account-multiple-outline'],
       ['Settings', 'mdi-cog-outline'],
